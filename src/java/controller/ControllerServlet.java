8e482dcc222765b0af6bee82a05982a81360d771
jl0p3z1 <jonathanglopez@gmail.com>
2016-11-30 21:39:29 -0500

    Servlet updated

--- src/java/controller/ControllerServlet.java
+++ src/java/controller/ControllerServlet.java
@@ -31,33 +31,6 @@ import javax.servlet.http.HttpServletResponse;
 public class ControllerServlet extends HttpServlet {
 
 	/**
-	 * Processes requests for both HTTP <code>GET</code> and
-	 * <code>POST</code> methods.
-	 *
-	 * @param request servlet request
-	 * @param response servlet response
-	 * @throws ServletException if a servlet-specific error occurs
-	 * @throws IOException if an I/O error occurs
-	 */
-	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException {
-		response.setContentType("text/html;charset=UTF-8");
-		try (PrintWriter out = response.getWriter()) {
-			/* TODO output your page here. You may use following sample code. */
-			out.println("<!DOCTYPE html>");
-			out.println("<html>");
-			out.println("<head>");
-			out.println("<title>Servlet ControllerServlet</title>");			
-			out.println("</head>");
-			out.println("<body>");
-			out.println("<h1>Servlet ControllerServlet at " + request.getContextPath() + "</h1>");
-			out.println("</body>");
-			out.println("</html>");
-		}
-	}
-
-	// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-	/**
 	 * Handles the HTTP <code>GET</code> method.
 	 *
 	 * @param request servlet request
@@ -68,7 +41,37 @@ public class ControllerServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 		throws ServletException, IOException {
-		processRequest(request, response);
+
+		String userPath = request.getServletPath();
+
+		// if category page is requested
+		if (userPath.equals("/category")) {
+			// TODO: Implement category request
+
+			// if cart page is requested
+		} else if (userPath.equals("/viewCart")) {
+			// TODO: Implement cart page request
+
+			userPath = "/cart";
+
+			// if checkout page is requested
+		} else if (userPath.equals("/checkout")) {
+			// TODO: Implement checkout page request
+
+			// if user switches language
+		} else if (userPath.equals("/chooseLanguage")) {
+			// TODO: Implement language request
+
+		}
+
+		// use RequestDispatcher to forward request internally
+		String url = "/WEB-INF/view" + userPath + ".jsp";
+
+		try {
+			request.getRequestDispatcher(url).forward(request, response);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 
 	/**
@@ -82,17 +85,32 @@ public class ControllerServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 		throws ServletException, IOException {
-		processRequest(request, response);
-	}
 
-	/**
-	 * Returns a short description of the servlet.
-	 *
-	 * @return a String containing servlet description
-	 */
-	@Override
-	public String getServletInfo() {
-		return "Short description";
-	}// </editor-fold>
+		String userPath = request.getServletPath();
+
+		// if addToCart action is called
+		if (userPath.equals("/addToCart")) {
+			// TODO: Implement add product to cart action
+
+			// if updateCart action is called
+		} else if (userPath.equals("/updateCart")) {
+			// TODO: Implement update cart action
+
+			// if purchase action is called
+		} else if (userPath.equals("/purchase")) {
+			// TODO: Implement purchase action
+
+			userPath = "/confirmation";
+		}
+
+		// use RequestDispatcher to forward request internally
+		String url = "/WEB-INF/view" + userPath + ".jsp";
+
+		try {
+			request.getRequestDispatcher(url).forward(request, response);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 
 }
